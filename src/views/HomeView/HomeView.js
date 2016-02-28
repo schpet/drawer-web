0f3ca751e464d8fda6a18343668ef20785d3a65d
Peter Schilling <peter@peterschilling.org>
2016-02-28 13:59:02 -0800

    Clean up homeview a bit

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,11 +1,10 @@
 /* @flow */
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { increment, doubleAsync } from '../../redux/modules/counter'
 import { fetchDocuments } from '../../redux/modules/documents'
 import { fetchUser } from '../../redux/modules/user'
-import DuckImage from './Duck.jpg'
-import classes from './HomeView.scss'
+// import DuckImage from './Duck.jpg'
+// import classes from './HomeView.scss'
 import Document from 'components/Document'
 import NewDocument from 'components/NewDocument'
 import Uri from 'jsuri'
@@ -18,10 +17,7 @@ import Uri from 'jsuri'
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
 export class HomeView extends React.Component {
   static propTypes = {
-    counter: PropTypes.number.isRequired,
     documents: PropTypes.object.isRequired,
-    doubleAsync: PropTypes.func.isRequired,
-    increment: PropTypes.func.isRequired,
     fetchDocuments: PropTypes.func.isRequired,
     fetchUser: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired
@@ -60,7 +56,6 @@ export class HomeView extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return ({
-    counter: state.counter,
     documents: state.documents,
     user: state.user,
     dispatch: ownProps.dispatch
@@ -68,8 +63,6 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 export default connect((mapStateToProps), {
-  increment: () => increment(1),
-  doubleAsync,
   fetchDocuments,
   fetchUser
 })(HomeView)
