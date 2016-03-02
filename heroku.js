0193dd63373d92d1bb0b49525fa8dfc18b47656a
Peter Schilling <peter@peterschilling.org>
2016-03-01 18:33:47 -0800

    switch to the most basic express setup i could find

--- heroku.js
+++ heroku.js
@@ -1,11 +1,22 @@
-import Koa from 'koa'
-import convert from 'koa-convert'
-import serve from 'koa-static'
+// from https://ninja.sg/spa-router-fallback/
+//
+// this is here, along with
+// "express": "^4.13.4",
+// "express-history-api-fallback": "^2.0.0",
+// in the package.json to let heroku run the app.
+//
+// hopefully in the future it will be replaced by some sort of
+// universal rendering setup.
 
-const app = new Koa()
-const PORT = 3444
+var fallback = require('express-history-api-fallback')
+var express = require('express')
+var app = express()
+var root = __dirname + '/dist'
+app.use(express.static(root))
+app.use(fallback('index.html', { root: root }))
 
-app.use(convert(serve(__dirname + '/dist')))
+var PORT = 4444
 app.listen(PORT)
 
-console.log(`listening on ${PORT}`)
+console.log("listening on " + PORT)
+
