c37fbb88f68bffaeb1cd52bcb052ecbc3cfe778a
Peter Schilling <peter@peterschilling.org>
2016-03-01 19:50:31 -0800

    fix heroku express server port config

--- heroku.js
+++ heroku.js
@@ -12,11 +12,11 @@ var fallback = require('express-history-api-fallback')
 var express = require('express')
 var app = express()
 var root = __dirname + '/dist'
+
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(root))
 app.use(fallback('index.html', { root: root }))
 
-var PORT = 4444
-app.listen(PORT)
-
-console.log("listening on " + PORT)
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
